2586c069c95e92bfcca248b4fe0d46c7a71f7ff9
Gideon Jacob <gideonjacob.in@gmail.com>
2025-03-28 07:55:36 +0000

    Added Polyline & drag edit feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,249 @@
 import { useState } from 'react'
-import { Map, AdvancedMarker, Pin} from '@vis.gl/react-google-maps';
+
+import { Map, AdvancedMarker, Pin } from '@vis.gl/react-google-maps';
 import type { MapCameraChangedEvent, MapMouseEvent } from '@vis.gl/react-google-maps';
 import { Polygon } from './components/Polygon';
+import { Polyline } from './components/Polyline';
+
+import { GrUndo as UndoIcon } from "react-icons/gr";
+import { Circles as LoaderSpinner } from 'react-loader-spinner';
 
 import './App.scss'
+
+const polylinePath = [
+  {
+      "lat": 12.999047907183801,
+      "lng": 80.27334355403984
+  },
+  {
+      "lat": 12.99917335391857,
+      "lng": 80.2733381896219
+  },
+  {
+      "lat": 12.999283119759468,
+      "lng": 80.27332746078595
+  },
+  {
+      "lat": 12.999432087608785,
+      "lng": 80.2733328252039
+  },
+  {
+      "lat": 12.999536626397004,
+      "lng": 80.27337305833852
+  },
+  {
+      "lat": 12.99949219741738,
+      "lng": 80.27339183380138
+  },
+  {
+      "lat": 12.999424247197892,
+      "lng": 80.27337305833852
+  },
+  {
+      "lat": 12.999360450336551,
+      "lng": 80.27336872594788
+  },
+  {
+      "lat": 12.999277892815766,
+      "lng": 80.27336769392058
+  },
+  {
+      "lat": 12.999220396427758,
+      "lng": 80.27337037612959
+  },
+  {
+      "lat": 12.999157673080196,
+      "lng": 80.27339183380138
+  },
+  {
+      "lat": 12.999094949716781,
+      "lng": 80.27343743135395
+  },
+  {
+      "lat": 12.999157673080196,
+      "lng": 80.27345888902576
+  },
+  {
+      "lat": 12.999233463789846,
+      "lng": 80.27342938472704
+  },
+  {
+      "lat": 12.999319708362336,
+      "lng": 80.27341597368216
+  },
+  {
+      "lat": 12.999398112493118,
+      "lng": 80.2734213381001
+  },
+  {
+      "lat": 12.99946344924987,
+      "lng": 80.27342938472704
+  },
+  {
+      "lat": 12.999528785989408,
+      "lng": 80.27344547798089
+  },
+  {
+      "lat": 12.99957321496248,
+      "lng": 80.27348839332448
+  },
+  {
+      "lat": 12.999549693742425,
+      "lng": 80.27352057983218
+  },
+  {
+      "lat": 12.999458222309958,
+      "lng": 80.27349375774243
+  },
+  {
+      "lat": 12.999392827236528,
+      "lng": 80.27347010991026
+  },
+  {
+      "lat": 12.99933277571918,
+      "lng": 80.27347498227961
+  },
+  {
+      "lat": 12.999272665871962,
+      "lng": 80.27347498227961
+  },
+  {
+      "lat": 12.999230850317465,
+      "lng": 80.27347766448858
+  },
+  {
+      "lat": 12.999209942537606,
+      "lng": 80.27349912216039
+  },
+  {
+      "lat": 12.999149832660626,
+      "lng": 80.2735018043694
+  },
+  {
+      "lat": 12.99907404192543,
+      "lng": 80.2734642534437
+  },
+  {
+      "lat": 12.999021772439411,
+      "lng": 80.2734240203091
+  },
+  {
+      "lat": 12.998982570317665,
+      "lng": 80.27337037612959
+  },
+  {
+      "lat": 12.998964275992066,
+      "lng": 80.27343474914498
+  },
+  {
+      "lat": 12.9989904107425,
+      "lng": 80.27350985099629
+  },
+  {
+      "lat": 12.999063588029133,
+      "lng": 80.27350716878735
+  },
+  {
+      "lat": 12.999126311400461,
+      "lng": 80.27354203750403
+  },
+  {
+      "lat": 12.999215169482731,
+      "lng": 80.27355813075788
+  },
+  {
+      "lat": 12.9992857332313,
+      "lng": 80.27354740192197
+  },
+  {
+      "lat": 12.999377204727327,
+      "lng": 80.27353935529504
+  },
+  {
+      "lat": 12.999450381899894,
+      "lng": 80.27353130866813
+  },
+  {
+      "lat": 12.999510491704074,
+      "lng": 80.27355276633992
+  },
+  {
+      "lat": 12.999588895774595,
+      "lng": 80.27359568168352
+  },
+  {
+      "lat": 12.999649005545237,
+      "lng": 80.27364664365409
+  },
+  {
+      "lat": 12.999669913288116,
+      "lng": 80.27373247434133
+  },
+  {
+      "lat": 12.999638551673112,
+      "lng": 80.27373515655026
+  },
+  {
+      "lat": 12.999601963117255,
+      "lng": 80.27366273690794
+  },
+  {
+      "lat": 12.999553337256929,
+      "lng": 80.27363507588622
+  },
+  {
+      "lat": 12.99946083577992,
+      "lng": 80.2735876350566
+  },
+  {
+      "lat": 12.99940333943429,
+      "lng": 80.27358227063864
+  },
+  {
+      "lat": 12.99935629695978,
+      "lng": 80.27358495284763
+  },
+  {
+      "lat": 12.9993040275332,
+      "lng": 80.27359568168352
+  },
+  {
+      "lat": 12.999230850317465,
+      "lng": 80.27361713935532
+  },
+  {
+      "lat": 12.999157673080196,
+      "lng": 80.27359299947454
+  },
+  {
+      "lat": 12.999092336242965,
+      "lng": 80.27357154180275
+  },
+  {
+      "lat": 12.999060974554972,
+      "lng": 80.27354203750403
+  },
+  {
+      "lat": 12.99902438591399,
+      "lng": 80.27358227063864
+  },
+  {
+      "lat": 12.999058361080786,
+      "lng": 80.27363323260921
+  },
+  {
+      "lat": 12.99912108445345,
+      "lng": 80.27363323260921
+  },
+  {
+      "lat": 12.999207329064983,
+      "lng": 80.273654690281
+  },
+  {
+    lat: 12.999290306806886,
+    lng: 80.27369224120666
+  }
+]
 
 function App() {
   const dronePosition = { lat: 12.999290306806886, lng: 80.27369224120666 };
@@ -13,8 +253,9 @@ function App() {
 
   const [isFinalized, setIsFinalized] = useState(false);
   const [isSent, setIsSent] = useState(false);
+  const [isConnectionEstablished, setIsConnectionEstablished] = useState(false);
 
-  const [color1, color2] = ['#005DAB', '#003366'];
+  const [color1, color2, color3] = ['#005DAB', '#003366', '#DF4641'];
 
   const handleZoomChanged = (event: MapCameraChangedEvent) => {
     const newZoom = event.map.getZoom();
@@ -29,14 +270,37 @@ function App() {
   const handleClick = (event: MapMouseEvent) => {
     const lat = event.detail.latLng?.lat;
     const lng = event.detail.latLng?.lng;
-    console.log('lat', lat, 'lng', lng);
 
     if (lat && lng) setBoundaryList((prev) => [...prev, { lat, lng }]);
   }
 
+  const handlePolygonClick = (event: google.maps.MapMouseEvent) => {
+    const lat = event.latLng?.lat();
+    const lng = event.latLng?.lng();
+
+    if (lat && lng) setBoundaryList((prev) => [...prev, { lat, lng }]);
+  }
+
+  const handleDrag = (index: number, event: google.maps.MapMouseEvent) => {
+    const lat = event.latLng?.lat();
+    const lng = event.latLng?.lng();
+    
+    if (lat && lng) {
+      setBoundaryList((prev) => {
+        const newList = [...prev];
+        newList[index] = { lat, lng };
+        return newList;
+      });
+    }
+  }
+
   const handleFinalize = () => {
+    if (boundaryList.length < 3) {
+      alert('Please select at least 3 points to create a polygon.');
+      return;
+    }
+
     setIsFinalized((prev) => !prev);
-    console.log(boundaryList);
   }
 
   const handleUndo = () => {
@@ -45,6 +309,12 @@ function App() {
       newList.pop();
       return newList;
     });
+  }
+
+  const handleSend = () => {
+    setIsSent(true);
+    setTimeout(() => setIsConnectionEstablished(true), 3000);
+    console.log(boundaryList);
   }
 
   return (
@@ -58,18 +328,26 @@ function App() {
         
           disableDefaultUI={true}
           zoomControl={true}
-          scaleControl={true}
-          rotateControl={true}
         
           onZoomChanged={handleZoomChanged}
           onCenterChanged={handleCenterChanged}
           {...isFinalized ? {} : { onClick: handleClick }}
         >
-          <AdvancedMarker position={dronePosition} ></AdvancedMarker>
-          <Polygon fillColor={color1} strokeColor={color2} paths={boundaryList} />
+          <AdvancedMarker position={dronePosition}></AdvancedMarker>
+          <Polygon
+            fillColor={color1}
+            strokeColor={color2}
+            paths={boundaryList}
+            {...isFinalized ? {} : { onClick: handlePolygonClick }}
+          />
 
           {boundaryList.map((boundary, index) => (
-            <AdvancedMarker key={index} position={boundary} >
+            <AdvancedMarker
+              key={index}
+              position={boundary}
+              draggable={!isFinalized}
+              {...isFinalized ? {} : { onDrag: (event) => handleDrag(index, event) }}
+            >
               <Pin
                 background={'transparent'}
                 borderColor={color1}
@@ -79,26 +357,47 @@ function App() {
               />
             </AdvancedMarker>
           ))}
+
+          {isSent && isConnectionEstablished && (
+            <Polyline paths={polylinePath} strokeColor={color3} />
+          )}
         </Map>
       </div>
 
-      <div className='btn-container'>
-        <div style={{ display: 'flex', gap: '10px' }}>
-          <button type='button' className='btn' onClick={handleFinalize} disabled={isSent}>
-            {isFinalized ? 'Edit Boundary' : 'Finalize Boundary'}
-          </button>
-
-          {isFinalized && (
-            <button type='button' className='btn' onClick={() => setIsSent(true)} disabled={isSent}>
-              {isSent ? 'Sent ✅' : 'Send'}
+      {!isSent ? (
+        <div className='btn-container'>
+          <div style={{ display: 'flex', gap: '10px' }}>
+            <button type='button' className='btn' onClick={handleFinalize} disabled={isSent}>
+              {isFinalized ? 'Edit Boundary' : 'Finalize Boundary'}
             </button>
-          )}
-        </div>
 
-        <button type='button' className='btn' onClick={handleUndo} disabled={isFinalized}>
-          Undo
-        </button>
-      </div>
+            {isFinalized && (
+              <button type='button' className='btn' onClick={handleSend} disabled={isSent}>
+                Send
+              </button>
+            )}
+          </div>
+
+          <button
+            type='button'
+            className='btn'
+            onClick={handleUndo}
+            disabled={isFinalized}
+            title="Undo"
+          >
+            <UndoIcon className='icon' />
+          </button>
+        </div>
+      ) : !isConnectionEstablished ? (
+        <LoaderSpinner
+          visible={isSent && !isConnectionEstablished}
+          height={55}
+          width={55}
+          color="#005DAB"
+        />
+      ) : (
+        <p>Progress: 48%</p>  
+      )}
     </div>
   )
 }
---
+++ src/components/Polyline.tsx
@@ -0,0 +1,140 @@
+import {
+    forwardRef,
+    useContext,
+    useEffect,
+    useImperativeHandle,
+    useMemo,
+    useRef
+  } from 'react';
+  
+  import {GoogleMapsContext } from '@vis.gl/react-google-maps';
+  
+  import type {Ref} from 'react';
+  
+  type PolylineEventProps = {
+    onClick?: (e: google.maps.MapMouseEvent) => void;
+    onDrag?: (e: google.maps.MapMouseEvent) => void;
+    onDragStart?: (e: google.maps.MapMouseEvent) => void;
+    onDragEnd?: (e: google.maps.MapMouseEvent) => void;
+    onMouseOver?: (e: google.maps.MapMouseEvent) => void;
+    onMouseOut?: (e: google.maps.MapMouseEvent) => void;
+  };
+  
+  type PolylineCustomProps = {
+    /**
+     * this is an encoded string for the path, will be decoded and used as a path
+     */
+    paths: google.maps.LatLng[] | google.maps.LatLngLiteral[] | google.maps.MVCArray<google.maps.LatLng>;
+    encodedPath?: string;
+    strokeColor?: string;
+  };
+  
+  export type PolylineProps = google.maps.PolylineOptions &
+    PolylineEventProps &
+    PolylineCustomProps;
+  
+  export type PolylineRef = Ref<google.maps.Polyline | null>;
+  
+  function usePolyline(props: PolylineProps) {
+    const {
+      onClick,
+      onDrag,
+      onDragStart,
+      onDragEnd,
+      onMouseOver,
+      onMouseOut,
+      paths,
+      ...polylineOptions
+    } = props;
+    // This is here to avoid triggering the useEffect below when the callbacks change (which happen if the user didn't memoize them)
+    const callbacks = useRef<Record<string, (e: unknown) => void>>({});
+    Object.assign(callbacks.current, {
+      onClick,
+      onDrag,
+      onDragStart,
+      onDragEnd,
+      onMouseOver,
+      onMouseOut
+    });
+  
+    const polyline = useRef(new google.maps.Polyline()).current;
+    // update PolylineOptions (note the dependencies aren't properly checked
+    // here, we just assume that setOptions is smart enough to not waste a
+    // lot of time updating values that didn't change)
+    useMemo(() => {
+      polyline.setOptions(polylineOptions);
+    }, [polyline, polylineOptions]);
+  
+    const map = useContext(GoogleMapsContext)?.map;
+  
+    // update the path with the encodedPath
+    useMemo(() => {
+      polyline.setPath(paths);
+    }, [polyline, paths]);
+
+    useEffect(() => {
+      const { strokeColor = 'black' } = props;
+      const strokeWeight = 12; // Increase stroke width
+      const strokeOpacity = 0.6; // Optional: Adjust stroke opacity
+
+      polyline.setOptions({ 
+        strokeColor, 
+        strokeWeight, 
+        strokeOpacity,
+      });
+    }, [polyline, props, props.strokeColor]);
+  
+    // create polyline instance and add to the map once the map is available
+    useEffect(() => {
+      if (!map) {
+        if (map === undefined)
+          console.error('<Polyline> has to be inside a Map component.');
+  
+        return;
+      }
+  
+      polyline.setMap(map);
+  
+      return () => {
+        polyline.setMap(null);
+      };
+    }, [map, polyline]);
+  
+    // attach and re-attach event-handlers when any of the properties change
+    useEffect(() => {
+      if (!polyline) return;
+  
+      // Add event listeners
+      const gme = google.maps.event;
+      [
+        ['click', 'onClick'],
+        ['drag', 'onDrag'],
+        ['dragstart', 'onDragStart'],
+        ['dragend', 'onDragEnd'],
+        ['mouseover', 'onMouseOver'],
+        ['mouseout', 'onMouseOut']
+      ].forEach(([eventName, eventCallback]) => {
+        gme.addListener(polyline, eventName, (e: google.maps.MapMouseEvent) => {
+          const callback = callbacks.current[eventCallback];
+          if (callback) callback(e);
+        });
+      });
+  
+      return () => {
+        gme.clearInstanceListeners(polyline);
+      };
+    }, [polyline]);
+  
+    return polyline;
+  }
+  
+  /**
+   * Component to render a polyline on a map
+   */
+  export const Polyline = forwardRef((props: PolylineProps, ref: PolylineRef) => {
+    const polyline = usePolyline(props);
+  
+    useImperativeHandle(ref, () => polyline, [polyline]);
+  
+    return null;
+  });
